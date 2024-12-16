f0d7a7043ddf2611faf58e515b1d7c511ca6d71d
hskhyl <155405525+hskhyl@users.noreply.github.com>
2024-12-16 19:36:21 +0900

    [Bronze V] Title: 사칙연산, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,13 @@
+A, B = map(int, input().split())
+
+result_1 = A + B
+result_2 = A - B
+result_3 = A * B
+result_4 = A // B
+result_5 = A % B
+
+print(result_1)
+print(result_2)
+print(result_3)
+print(result_4)
+print(result_5)
