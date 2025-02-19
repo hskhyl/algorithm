b18ec54f2b43d283692d4fbdbb331626c011671c
hskhyl <155405525+hskhyl@users.noreply.github.com>
2025-02-19 11:15:30 +0900

    [Silver III] Title: N과 M (5), Time: 144 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ Python/백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,34 +1,13 @@
 import sys
-from copy import deepcopy
-from itertools import combinations
-
-def generate_combination(N:int, M:int, num_list:list[int]):  
-    visited = [False] * N
-    result = [] 
-        
-    def back_track(depth):
-        if depth == M:
-            print(*result)
-            return
-    
-        for i in range(N):
-            if not visited[i]:
-                visited[i] = True
-                result.append(num_list[i])
-                back_track(depth + 1)
-                result.pop()
-                visited[i] = False
-    
-    back_track(0)
-        
-            
+from itertools import permutations
 
 def main():
     N, M = map(int, sys.stdin.readline().rstrip().split())
     num_list = sorted(list(map(int, sys.stdin.readline().rstrip().split())))
-    generate_combination(N, M, num_list)
     
-
+    for permutation in permutations(num_list, M):
+        print(*permutation)
+    
 
 if __name__ == "__main__":
     main()
