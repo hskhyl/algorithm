8d55486989f81bac5c5f0f9fe3639398b9e9b5c3
hskhyl <155405525+hskhyl@users.noreply.github.com>
2025-02-24 16:12:29 +0900

    [Silver II] Title: N과 M (9), Time: 96 ms, Memory: 37024 KB -BaekjoonHub

--- Python/백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ Python/백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,30 +1,18 @@
 import sys
+from itertools import permutations
 
 def main():
     N, M = map(int, sys.stdin.readline().rstrip().split())
     num_list = sorted(list(map(int, sys.stdin.readline().rstrip().split())))
-    visited = [False] * N
-    combination = []
-    combination_set = set()
     
-    def back_track(depth):
-        if depth == M:
-            comb_tuple = tuple(combination)
-            if comb_tuple not in combination_set:
-                combination_set.add(comb_tuple)
-                print(*combination)
-            return
-            
-        for index in range(len(num_list)):
-            if not visited[index]:
-                visited[index] = True
-                combination.append(num_list[index])
-                back_track(depth + 1)
-                combination.pop()
-                visited[index] = False
+    combinations = permutations(num_list, M)
+    previous = set()
     
-    back_track(0)
-            
+    for combination in combinations:
+        if combination not in previous:
+            print(*combination)
+            previous.add(combination)
+
 
 if __name__ == "__main__":
     main()
